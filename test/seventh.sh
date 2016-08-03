d0eaa1d6b0b85dcd4ce79e7cfc0a4299e9619be5
Mukul Sharma <mukul.sharma@weboniselab.com>
2016-08-03 20:27:31 +0530

    Cleanup

--- test/seventh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp -R public_html public_html_renamed
-cp -R public_html_renamed public_html
-
-mv public_html/public_html_renamed public_html/public_html
-
-rm -rf public_html_renamed
